e93f265deb60db695dacbf4404da6c6457a62a1f
Paweł Nowak <pawel834@gmail.com>
2014-05-29 18:40:04 +0200

    Update the app to use new classifier.

--- src/wikiimporter/WikiImporter.java
+++ src/wikiimporter/WikiImporter.java
@@ -5,11 +5,23 @@ import java.io.IOException;
 import java.util.Collection;
 import javax.xml.parsers.ParserConfigurationException;
 import org.xml.sax.SAXException;
-import pl.edu.mimuw.wikiontology.pn347193.analysis.CategoryExtractor;
+import pl.edu.mimuw.wikiontology.pn347193.Article;
+import pl.edu.mimuw.wikiontology.pn347193.ArticleConsumer;
 import pl.edu.mimuw.wikiontology.pn347193.Ontology;
 import pl.edu.mimuw.wikiontology.pn347193.OntologyCollectionBuilder;
+import pl.edu.mimuw.wikiontology.pn347193.analysis.LinkExtractor;
+import pl.edu.mimuw.wikiontology.pn347193.analysis.PhysicistClassifier;
 import pl.edu.mimuw.wikiontology.pn347193.filters.PersonFilter;
 import pl.edu.mimuw.wikiontology.pn347193.importers.xml.XMLImporter;
+
+class ArticleTitlePrinter implements ArticleConsumer {
+    
+    @Override
+    public void accept(Article article) {
+        System.out.printf("Filtered out article %s.\n", article.getTitle());
+    }
+    
+}
 
 public class WikiImporter {
 
@@ -21,11 +33,13 @@ public class WikiImporter {
             System.out.println("Usage: WikiImporter <xml file>");
             return;
         }
-
+        
         OntologyCollectionBuilder builder = new OntologyCollectionBuilder();
         builder.addFilter(PersonFilter.getInstance());
-        builder.addAnalysis(CategoryExtractor.getInstance());
-
+        builder.addAnalysis(LinkExtractor.getInstance());
+        builder.addAnalysis(PhysicistClassifier.getInstance());
+        builder.setFilteredArticleConsumer(new ArticleTitlePrinter());
+        
         try {
             XMLImporter importer = new XMLImporter(builder);
             FileInputStream input = new FileInputStream(args[0]);
@@ -34,13 +48,20 @@ public class WikiImporter {
             System.out.println("Cannot read the xml file.");
             System.out.println(e.getLocalizedMessage());
         }
-
+        
         Collection<Ontology> ontologies = builder.build();
         System.out.println(String.format("Imported %d ontologies.", ontologies.
             size()));
         for (Ontology onto : ontologies) {
-            System.out.println(onto.getTitle());
+            System.out.print(onto.getTitle());
+            boolean isPhysicist = onto.getAnalysisResult(PhysicistClassifier.
+                getInstance());
+            if (isPhysicist) {
+                System.out.println(" is a physicist.");
+            } else {
+                System.out.println(" is not a physicist.");
+            }
         }
     }
-
+    
 }
