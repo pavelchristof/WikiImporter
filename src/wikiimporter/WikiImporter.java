ea999acb8a4817a0368c8a13bf12e92184d8dddf
Paweł Nowak <pawel834@gmail.com>
2014-05-30 16:47:37 +0200

    Update to match library changes.

--- src/wikiimporter/WikiImporter.java
+++ src/wikiimporter/WikiImporter.java
@@ -2,25 +2,29 @@ package wikiimporter;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collection;
 import javax.xml.parsers.ParserConfigurationException;
 import org.xml.sax.SAXException;
 import pl.edu.mimuw.wikiontology.pn347193.Article;
 import pl.edu.mimuw.wikiontology.pn347193.ArticleConsumer;
-import pl.edu.mimuw.wikiontology.pn347193.Ontology;
-import pl.edu.mimuw.wikiontology.pn347193.OntologyCollectionBuilder;
+import pl.edu.mimuw.wikiontology.pn347193.Entity;
+import pl.edu.mimuw.wikiontology.pn347193.Identifier;
+import pl.edu.mimuw.wikiontology.pn347193.OntologyBuilder;
 import pl.edu.mimuw.wikiontology.pn347193.analysis.LinkExtractor;
 import pl.edu.mimuw.wikiontology.pn347193.analysis.PhysicistClassifier;
 import pl.edu.mimuw.wikiontology.pn347193.filters.PersonFilter;
 import pl.edu.mimuw.wikiontology.pn347193.importers.xml.XMLImporter;
+import pl.edu.mimuw.wikiontology.pn347193.relations.IsA;
+import pl.edu.mimuw.wikiontology.pn347193.relations.Relation;
 
 class ArticleTitlePrinter implements ArticleConsumer {
-    
+
     @Override
     public void accept(Article article) {
         System.out.printf("Filtered out article %s.\n", article.getTitle());
     }
-    
+
 }
 
 public class WikiImporter {
@@ -33,13 +37,13 @@ public class WikiImporter {
             System.out.println("Usage: WikiImporter <xml file>");
             return;
         }
-        
-        OntologyCollectionBuilder builder = new OntologyCollectionBuilder();
+
+        OntologyBuilder builder = new OntologyBuilder();
         builder.addFilter(PersonFilter.getInstance());
         builder.addAnalysis(LinkExtractor.getInstance());
         builder.addAnalysis(PhysicistClassifier.getInstance());
         builder.setFilteredArticleConsumer(new ArticleTitlePrinter());
-        
+
         try {
             XMLImporter importer = new XMLImporter(builder);
             FileInputStream input = new FileInputStream(args[0]);
@@ -48,20 +52,26 @@ public class WikiImporter {
             System.out.println("Cannot read the xml file.");
             System.out.println(e.getLocalizedMessage());
         }
-        
-        Collection<Ontology> ontologies = builder.build();
-        System.out.println(String.format("Imported %d ontologies.", ontologies.
+
+        ArrayList<Entity> ontology = builder.build();
+        System.out.println(String.format("Imported %d ontologies.", ontology.
             size()));
-        for (Ontology onto : ontologies) {
-            System.out.print(onto.getTitle());
-            boolean isPhysicist = onto.getAnalysisResult(PhysicistClassifier.
-                getInstance());
+        for (Entity entity : ontology) {
+            System.out.print(entity.getIdentifier().toString());
+            boolean isPhysicist = entity.hasRelation(new IsA(
+                Identifier.PHYSICIST));
             if (isPhysicist) {
                 System.out.println(" is a physicist.");
             } else {
                 System.out.println(" is not a physicist.");
             }
         }
+        
+        Entity entity = ontology.get(0);
+        System.out.println(entity.getIdentifier().toString());
+        for (Relation rel : entity.getRelations()) {
+            System.out.println("    " + rel.toString());
+        }
     }
-    
+
 }
